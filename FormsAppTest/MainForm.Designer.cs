1c6c81ca9ba212eb5ffc26ca1e13e96e9f9302ba
Luis Harvey Triana Vega <harveytriana@hotmail.com>
2020-11-07 00:34:44 -0500

    Commit 071120-0030

--- FormsAppTest/MainForm.Designer.cs
+++ FormsAppTest/MainForm.Designer.cs
@@ -33,29 +33,33 @@
             // 
             // labelTime
             // 
-            this.labelTime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelTime.BackColor = System.Drawing.Color.Salmon;
+            this.labelTime.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelTime.ForeColor = System.Drawing.Color.LavenderBlush;
             this.labelTime.Location = new System.Drawing.Point(28, 22);
             this.labelTime.Name = "labelTime";
             this.labelTime.Size = new System.Drawing.Size(200, 40);
             this.labelTime.TabIndex = 0;
-            this.labelTime.Text = "00:00:00.0000";
+            this.labelTime.Text = "Sensor Time";
             this.labelTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // labelValue
             // 
-            this.labelValue.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.labelValue.Location = new System.Drawing.Point(28, 73);
+            this.labelValue.BackColor = System.Drawing.Color.DarkSalmon;
+            this.labelValue.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelValue.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.labelValue.Location = new System.Drawing.Point(28, 64);
             this.labelValue.Name = "labelValue";
             this.labelValue.Size = new System.Drawing.Size(200, 40);
             this.labelValue.TabIndex = 0;
-            this.labelValue.Text = "00:00:00.0000";
+            this.labelValue.Text = "Sensor Value";
             this.labelValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 142);
+            this.ClientSize = new System.Drawing.Size(251, 133);
             this.Controls.Add(this.labelValue);
             this.Controls.Add(this.labelTime);
             this.Name = "MainForm";
